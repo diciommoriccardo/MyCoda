b45529536c157c79989e50311e6068b6f5e7f9db
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-10-04 20:35:55 +0200

    Added https listening server.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+import http from 'http';
+import https from 'https';
 import express from 'express';
-const app = express();
 import MongoDb from './src/helpers/MongoDb';
 import Router from './src/helpers/Router';
+import getCertificate from './src/utils/ssl/getCertificate';
 import { SERVER } from './src/config/config';
 
-app.use(express.json());
-new MongoDb();
-new Router(app).setAllRoutes();
+class WebServer {
+    constructor(certificate) {
+        this.certificate = certificate;
+        this.app = express();
+        this.app.use(express.json());
+        this.router = new Router(this.app).setAllRoutes();
+        this.mongodb = new MongoDb();
+        this.http = http.Server(this.app);
+        if (certificate)
+            this.https = https.Server(certificate, this.app);
+    }
 
-app.listen(SERVER.PORT, SERVER.HOST, () => {
-    console.log(`Listening on http://${SERVER.HOST}:${SERVER.PORT}`);
-})
+    listen() {
+        this.http.listen(SERVER.PORT, SERVER.HOST, () => {
+            console.log(`Listening on http://${SERVER.HOST}:${SERVER.PORT}`);
+        });
+        if (this.https)
+            this.https.listen(SERVER.HOST_SECURE, SERVER.PORT_SECURE, () => {
+                console.log(`Listening on https://${SERVER.HOST_SECURE}:${SERVER.PORT_SECURE}`);
+            });               
+    }
+}
+
+new WebServer(getCertificate()).listen();
--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,8 @@ console.log(`Server environment is ${environment}.`);
 const SERVER = {
   HOST: process.env.HOST || '0.0.0.0',
   PORT: process.env.PORT || '8080',
+  HOST_SECURE: process.env.HOST_SECURE || '0.0.0.0',
+  PORT_SECURE: process.env.PORT_SECURE || '8433',
 };
 
 const DATABASE = {
