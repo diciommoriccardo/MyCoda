a389203a00b7931f29de0c1d112c61dd9cbeefc8
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-10-04 20:36:24 +0200

    Changed mongodb condition from dev to prod.

--- src/helpers/MongoDb.js
+++ src/helpers/MongoDb.js
@@ -4,9 +4,9 @@ import { DATABASE } from '../config/config';
 class MongoDb {
   constructor() {
     this.db = mongoose.connection;
-    this.uri = (process.env.NODE_ENV === 'development')
-      ? `mongodb://${DATABASE.HOST}:${DATABASE.PORT}/${DATABASE.NAME}`
-      : DATABASE.URI;
+    this.uri = (process.env.NODE_ENV === 'production')
+      ? DATABASE.URI
+      : `mongodb://${DATABASE.HOST}:${DATABASE.PORT}/${DATABASE.NAME}`;
     this.db.on('open', () => { console.log(`Connected to ${this.uri}`); });
     this.db.on('reconnected', () => { console.log(`Reconnected to ${this.uri}`); });
     this.db.on('disconnected', () => { console.log(`Disconnected from ${this.uri}`); });
