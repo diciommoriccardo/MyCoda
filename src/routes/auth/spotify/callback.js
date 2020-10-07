d9ace75aaa93727063a94ea119e704b95452c69a
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-10-07 12:08:46 +0200

    Split spotify auth route in more files.

---
+++ src/routes/auth/spotify/callback.js
@@ -0,0 +1,36 @@
+import Router from 'express';
+import cookieParser from 'cookie-parser';
+import { SPOTIFY } from '../../../config/constants';
+import UsersHelper from '../../../helpers/Users';
+import jwt from '../../../helpers/Jwt';
+import spotifyApi from '../../../helpers/SpotifyApi';
+
+const router = Router().use(cookieParser());
+
+router.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[SPOTIFY.STATE.NAME] : null;
+
+    if (code === null || state === null) return res.status(400).json({ error: 'Invalid Request' });
+    if (state !== storedState) return res.status(403).json({ error: 'Forbidden' });
+    else res.clearCookie(SPOTIFY.STATE.NAME);
+
+    spotifyApi.getProfileFromCodeGrant(req.query.code)
+        .then((profile) => {
+            return new UsersHelper({
+                username: profile.id,
+                email: profile.email,
+            }).findOrSave();
+        })
+        .then(({ _id, username, email }) => {
+            const bearer_token = jwt.sign({ _id });
+            return res.status(201).json({ username, email, bearer_token });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).send({ error: 'Internal Server Error' });
+        })
+});
+
+export default router;
