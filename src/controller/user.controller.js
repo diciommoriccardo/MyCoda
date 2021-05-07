0df717d1b7850c1eb5ba270a4bf054c9c1f02feb
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-05-07 22:19:44 +0200

    add controller for login and register

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,40 @@
-import user from '../models/user.model.js';
+import User from '../models/user.model.js';
+import jwt from '../helpers/jwt.js';
+import ERRORS from '../config/constants.js';
 
 const controller = {
     findAll: (req, res) => {
-        console.log("findAll");
+        
+    },
+    login: (req, res) => {
+        if(!req.body){ res.status(400).send({ message: 'Content cannot be empty'})}
+        if(!req.body.cf || !req.body.password){ res.status(400).send({ message: 'Cf and password are required'})}
+
+        var user = new User({
+            cf: req.body.cf,
+            password: req.body.password
+        })
+
+        user.login()
+        .then( (result) => {
+            var userAccessToken = jwt.signAccessToken(result.cf)
+            res.status(201).send(userAccessToken);
+        })
+        .catch( (err) => {res.status(500).send({ message: ERRORS.LOGIN})})
+    },
+    findByCf: (req, res) => {
+        if(!req.body){res.status(400).send({ message: 'Content cannot be empty'})}
+        if(!req.body.cf){ res.status(400).send({ message: 'Cf are required'})}
+
+        var user = new User({
+            cf: req.body.cf
+        })
+
+        user.findByCf(cf)
+        .then( (result) =>{
+            res.status(201).send(result)
+        })
+        .catch( (err) => {res.status(500).send({ message: err})})
     }
 }
 
