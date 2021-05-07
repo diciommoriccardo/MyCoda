9b5bc6d2d995d209823b884271c317d9ba6227fd
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-05-07 22:31:02 +0200

    indentation

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,6 +3,7 @@ import jwt from '../helpers/jwt.js';
 import JWT from '../config/constants.js';
 import ERRORS from '../config/constants.js';
 import controller from '../controller/user.controller.js'
+
 const router = Router();
 
 router.post('/users/login', controller.login);
