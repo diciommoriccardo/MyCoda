d6f60c2d74ef0cbcba1c88806ca625fd5ccca491
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-06-07 22:31:32 +0200

    Added all the api tree under the /api route

--- src/helpers/Router.js
+++ src/helpers/Router.js
@@ -1,23 +1,25 @@
 import authMiddleware from '../middlewares/auth.js';
 import tokenRenewal from '../routes/token.js';
-import getUser from '../routes/user/getUser.js';
+import getUser from '../routes/user/getById.js';
 import userLogin from '../routes/user/login.js';
 import userRegister from '../routes/user/register.js';
-import getPharma from '../routes/pharma/getPharma.js';
-import pharmaLogin from '../routes/pharma/login.js';
-import pharmaRegister from '../routes/pharma/register.js';
+import getPharmacy from '../routes/pharmacy/getById.js';
+import pharmacyLogin from '../routes/pharmacy/login.js';
+import pharmacyRegister from '../routes/pharmacy/register.js';
 import notFoundMiddleware from '../middlewares/notFound.js';
-import getAll from '../routes/pharma/getAll.js';
+import getAll from '../routes/pharmacy/getAll.js';
 
 class Router {
     constructor( app ){
         this.app = app;
         this.routerSchema = {
-            '/api': {
-                "/token" : tokenRenewal
-            },
-            '/users': [getUser,userLogin,userRegister],
-            '/pharma': [getPharma,pharmaLogin,pharmaRegister,getAll],
+            '/api': [
+                tokenRenewal,
+                {
+                    '/users': [userLogin, userRegister, authMiddleware, getUser],
+                    '/pharma': [getPharmacy, pharmacyLogin, pharmacyRegister, getAll],
+                }
+            ],
             '*': notFoundMiddleware,
         }
     }
