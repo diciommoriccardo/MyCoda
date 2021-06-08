70fbb158085886890b1ef622487b9df1000107e4
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-06-08 16:28:20 +0200

    Fixed dotenv import

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config({path: 'process.env' });
+dotenv.config();
 
 const environment = process.env.NODE_ENV || 'development';
 console.log(`Server environment is ${environment}.`);
