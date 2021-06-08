801723d2a70b57d7dffcdce7ad771880013e0af1
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-06-08 16:02:55 +0200

    Fixed promise return

--- src/routes/user/getById.js
+++ src/routes/user/getById.js
@@ -12,7 +12,7 @@ router.get('/:cf', function(req, res) {
     if (!req.body.cf) return res.status(400).json({error: {message: 'Cf is required'}});
 
     new User({cf: req.body.cf})
-    .then(user => { user.findByCf(user.cf) })
+    .then(user => user.findByCf(user.cf))
     .then(result =>{ return res.status(201).json({message: SUCCESS_ITA.DEFAULT, result: result})})
     .catch(err => { return res.status(500).json({error: { message: err}})})
 });
