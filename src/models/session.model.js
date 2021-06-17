d1bf019e4fda256cdfe6df63efc3442cfc89b336
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-17 16:54:32 +0200

    added fetch methods

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -2,9 +2,13 @@ import pool from '../helpers/mysql.js';
 
 class Session {
     constructor(session){
-        this.cfUtente = session.cfUtente
-        this.time = session.time
-        this.pivaFarm = session.pivaFarm
+        return new Promise( (resolve) =>{
+            this.cfUtente = session.cfUtente
+            this.time = Math.floor(Date.now())
+            this.pivaFarm = session.pivaFarm
+            resolve(this)
+        })
+        
     }
 
     create(){
@@ -20,6 +24,24 @@ class Session {
 
                         connection.release()
                         resolve(this.values)
+                    })
+            })
+        })
+    }
+
+    findById(){
+        return new Promise( (resolve, reject) =>{
+            let sql = "SELECT * FROM msg WHERE cfUtente = ? AND pivaFarm = ?";
+
+            pool.getConnection( (err, connection) =>{
+                if(err) return reject(err);
+
+                connection.query(sql, this.cfUtente, this.pivaFarm, 
+                    function(err, result){
+                        if(err) return reject(err);
+                        
+                        connection.release();
+                        resolve(result);
                     })
             })
         })
@@ -61,3 +83,5 @@ class Session {
         })
     }
 }
+
+export default session
