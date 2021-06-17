716010dc8bdef5ca9d3b95d3a6c336547e2b8fc4
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-17 16:58:28 +0200

    fixed method

--- src/routes/pharmacy/getById.js
+++ src/routes/pharmacy/getById.js
@@ -3,12 +3,12 @@ import Pharmacy from '../../models/pharmacy.model.js';
 
 const router = Router();
 
-router.get('/:piva', function(req, res) {
+router.get('/:id', function(req, res) {
     if(!req.body) return res.status(400).json({ error: { message:'Content cannot be empty'} });
     if(!req.body.piva) return res.status(400).json({ error: { message: 'P.IVA are required'} });
 
-    new Pharmacy(req.body)
-        .then(pharmacy => pharmacy.findByCf(pharmacy.piva))
+    new Pharmacy({piva: req.params.id})
+        .then(pharmacy => pharmacy.findByCf())
         .then(result => { return res.status(201).json(result); })
         .catch(err => { return res.status(500).json({ error: { message: err } }); });
 });
