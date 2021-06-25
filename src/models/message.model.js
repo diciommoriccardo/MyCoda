8fdf52b8f20c785ccdd167f6076b6208e5f2975a
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-25 10:22:52 +0200

    message model

---
+++ src/models/message.model.js
@@ -0,0 +1,88 @@
+import pool from '../helpers/mysql.js';
+
+class Message {
+    constructor(message){
+        return new Promise( (resolve) =>{
+            this.cfUtente = session.cfUtente
+            this.time = message.time
+            this.content = message.content
+            this.pivaFarm = session.pivaFarm
+            resolve(this)
+        })
+        
+    }
+
+    create(){
+        return new Promise( (resolve, reject) =>{
+            let sql = "INSERT INTO msg SET ?"
+
+            pool.getConnection( (err, connection) =>{
+                if(err) return reject(err)
+
+                connection.query(sql, [this],
+                    function(err){
+                        if(err) return reject(err)
+
+                        connection.release()
+                        resolve(this.values)
+                    })
+            })
+        })
+    }
+
+    findById(){
+        return new Promise( (resolve, reject) =>{
+            let sql = "SELECT * FROM msg WHERE cfUtente = ? AND pivaFarm = ?";
+
+            pool.getConnection( (err, connection) =>{
+                if(err) return reject(err);
+
+                connection.query(sql, [connection.escape(this.cfUtente),connection.escape(this.pivaFarm)], 
+                    function(err, result){
+                        if(err) return reject(err);
+                        
+                        connection.release();
+                        resolve(result);
+                    })
+            })
+        })
+    }
+
+    findByUser(){
+        return new Promise( (resolve, reject) =>{
+            let sql = "SELECT * FROM msg WHERE cfUtente = ?"
+
+            pool.getConnection( (err, connection) =>{
+                if(err) return reject(err)
+
+                connection.query(sql, connection.escape(this.cfUtente), 
+                    function(err, result){
+                        if(err) return reject(err)
+
+                        connection.release()
+                        resolve(result)
+                    })
+            })
+        })
+    }
+
+    findByPharma(){
+        return new Promise( (resolve, reject) =>{
+            let sql = "SELECT * FROM msg WHERE pivaFarm = ?"
+
+            pool.getConnection((err, connection) =>{
+                if(err) return reject(err)
+
+                connection.query(sql, connection.escape(this.pivaFarm),
+                    function(err, result){
+                        if(err) return reject(err)
+
+                        connection.release()
+                        resolve(result)
+                    })
+            })
+        })
+    }
+}
+
+export default Message
