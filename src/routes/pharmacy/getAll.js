6314f83e0f0467b0b3b977abd95b34ab78ad53a9
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-30 18:35:31 +0200

    fixed output

--- src/routes/pharmacy/getAll.js
+++ src/routes/pharmacy/getAll.js
@@ -6,7 +6,7 @@ const router = new Router();
 router.get('/', (req, res) => {
     new Pharmacy()
         .then(pharmacy => pharmacy.getAll())
-        .then(result => { return res.status(201).json(result); })
+        .then(result => { return res.status(201).json({pharmacies: result}); })
         .catch(err => { return res.status(500).json({ error: { message: err } }); });
 })
 
