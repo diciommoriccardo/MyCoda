6c939c5e6f07a5162c4cb0aea81c6ab7942f0025
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-30 18:34:36 +0200

    fixed date and sql

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -2,15 +2,18 @@ import pool from '../helpers/mysql.js';
 
 class Payment {
     constructor(payment){
-        this.somma = payment.somma
-        this.time = Math.floor(Date.now())
-        this.cfUtente = payment.cfUtente
-        this.pivaFarm = payment.pivaFarm
+        return new Promise((resolve, reject) => {
+            this.somma = payment.somma
+            this.time = new Date(Date.now())
+            this.cfUtente = payment.cfUtente
+            this.pivaFarm = payment.pivaFarm
+            resolve(this)
+        })
     }
 
     create(){
         return new Promise( (resolve, reject) =>{
-            let sql = "INSERT INTO farma SET ?";
+            let sql = "INSERT INTO payment SET ?";
 
             pool.getConnection( (err, connection) =>{
                 if(err) return reject(err)
@@ -28,12 +31,12 @@ class Payment {
 
     findByUser(){
         return new Promise( (resolve, reject) =>{
-            let sql = "SELECT * FROM payment WHERE cfUtente = ?"
+            let sql = "SELECT * FROM payment WHERE `cfUtente` = ?"
 
             pool.getConnection( (err, connection) =>{
                 if(err) return reject(err)
 
-                connection.query(sql, this.cfUtente,
+                connection.query(sql, [this.cfUtente],
                     function(err, result){
                         if(err) return reject(err)
 
