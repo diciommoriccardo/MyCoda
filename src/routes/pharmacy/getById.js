1fa68d6451ad4c1a18a8cdfffeac3d58d67e1bf7
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-06-30 18:35:54 +0200

    fixed output and params

--- src/routes/pharmacy/getById.js
+++ src/routes/pharmacy/getById.js
@@ -3,13 +3,13 @@ import Pharmacy from '../../models/pharmacy.model.js';
 
 const router = Router();
 
-router.get('/:id', function(req, res) {
+router.get('/me', function(req, res) {
     if(!req.body) return res.status(400).json({ error: { message:'Content cannot be empty'} });
     if(!req.body.piva) return res.status(400).json({ error: { message: 'P.IVA are required'} });
 
-    new Pharmacy({piva: req.params.id})
+    new Pharmacy({piva: req.user.piva})
         .then(pharmacy => pharmacy.findByCf())
-        .then(result => { return res.status(201).json(result); })
+        .then(result => { return res.status(201).json({pharmacy: result}); })
         .catch(err => { return res.status(500).json({ error: { message: err } }); });
 });
 
