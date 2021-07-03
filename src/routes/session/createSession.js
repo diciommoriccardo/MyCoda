3a9d9dfece59d72c20eec9e598462a8e5cb646a0
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-03 15:07:19 +0200

    Fixed create session route

--- src/routes/session/createSession.js
+++ src/routes/session/createSession.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post('/:id', (req, res) => {
     new Session({
-        pivaFarm: req.params.id,
+        pivaFarma: req.params.id,
         cfUtente: req.user.id
     })
     .then(session => session.create())
