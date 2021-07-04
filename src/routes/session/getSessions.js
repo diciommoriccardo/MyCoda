2ebfd6c99b43f10df360e4c790d3ad4aa54ef6ae
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-04 18:17:59 +0200

    Improved code

--- src/routes/session/getSessions.js
+++ src/routes/session/getSessions.js
@@ -1,16 +1,14 @@
 import Router from 'express';
 import Session from '../../models/session.model.js';
-import {SUCCESS_ITA} from '../../config/constants.js';
 
 const router = Router();
 
 router.get('', (req, res) => {
-    const type = req.user.type;
-    const id = req.user.id;
+    const { id, type } = req.user;
     const session = {
-        ...(type === 'user' ? { cfUtente: id } : { pivaFarma: id} )
+        ...(type === 'user' ? { cfUtente: id } : { pivaFarma: id })
     };
-
+    
     new Session( session )
     .then(session => type === 'user' ? session.findByUser() : session.findByPharma())
     .then(result => { return res.status(201).json(result); })
