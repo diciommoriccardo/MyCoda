d3ecf2f66aa14f794c0447755e6d4ea29a613f6d
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-04 18:17:48 +0200

    Improverd code

--- src/routes/session/getOpenSession.js
+++ src/routes/session/getOpenSession.js
@@ -1,29 +1,18 @@
 import Router from 'express';
 import Session from '../../models/session.model.js';
-import {SUCCESS_ITA} from '../../config/constants.js';
 
 const router = Router();
 
 router.get('/open', (req, res) => {
-    const type = req.user.type
-    
-    switch (type) {
-        case 'pharmacy':
-            var session = {
-                pivaFarma: req.user.id
-            }
-            break;
-        case 'user':
-            var session = {
-                cfUtente: req.user.id
-            }
-            break;
-    }
+    const { id, type } = req.user;
+    const session = {
+        ...(type === 'user' ? { cfUtente: id } : { pivaFarma: id })
+    };
 
     new Session( session )
-    .then(session => type == 'user' ? session.findOpenSessionByUser() : session.findOpenSessionByPharma())
-    .then(result => { console.log(result); return res.status(201).json({result: result, message: SUCCESS_ITA.DEFAULT})})
-    .catch(err => { return res.status(500).json({error: {message: err}})})
+    .then(session => type === 'user' ? session.findOpenSessionByUser() : session.findOpenSessionByPharma())
+    .then(result => { return res.status(201).json(result)})
+    .catch(err => { return res.status(500).json({error: { message: err }})})
 
 })
 
