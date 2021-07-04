2d6a9c1f30ad85e5a5ec3ca08dc6093cebd31e05
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-04 18:19:08 +0200

    Improved but still not functioning

--- src/routes/session/saveMessageWithSession.js
+++ src/routes/session/saveMessageWithSession.js
@@ -6,34 +6,24 @@ import {SUCCESS_ITA} from '../../config/constants.js';
 const router = Router();
 
 router.post('/:id/message', (req, res) => {
+    if (!req.body.content || req.body.content == '') return res.status(400).json({ error: { message: 'Content cannot be empty' } });
     const type = req.user.type;
     const id = req.user.id;
     const session = {
         ...(type === 'user' ? { cfUtente: id, pivaFarma: req.params.id } : { pivaFarma: id, cfUtente: req.params.id} )
     };
 
-    console.log(session)
-
-    const sessione = new Session( session )
-    .then((session) => session.findOpenSessionById())
-    .then((result) => {
-        console.log(result)
-        if(result.length == 0){return sessione.create()}
-
-        return result
-    })
-    .then((result) =>{
-        console.log(result)
-        new Message({
+    new Session( session )
+        .then(session => session.findOpenSessionById())
+        .then(result => result.length === 0 ? new Session(session).create() : result)
+        .then(result => new Message({
             cfUtente: result[0].cfUtente,
             pivaFarma: result[0].pivaFarma,
             content: req.body.content
-        })
-        .then((message) => {console.log(message); return message.create()})
-        .catch(err => req.status(500).json({error: {message: err}}))
-    })
-    .then((result) => res.status(201).json(result))
-    .catch(err => { return res.status(500).json({error: {message: err}})})
+        }))
+        .then((message) => { console.log(message); return message.create() })
+        .then((result) => res.status(201).json(result))
+        .catch(err => { return res.status(500).json({error: {message: err}})})
 })
 
 export default router
