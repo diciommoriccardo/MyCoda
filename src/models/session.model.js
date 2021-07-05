2e91a9b2e60977d1137570c6a61291d595b73116
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-06 00:54:25 +0200

    added id

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -3,6 +3,7 @@ import pool from '../helpers/mysql.js';
 class Session {
     constructor(session){
         return new Promise( (resolve) =>{
+            this.id = session.id
             this.cfUtente = session.cfUtente
             this.time = new Date(Date.now())
             this.pivaFarma = session.pivaFarma
@@ -17,33 +18,25 @@ class Session {
             pool.getConnection((err, connection) => {
                 if(err) reject(err)
 
-                let sqlExist = 'SELECT * FROM farma WHERE piva = ?';
-
-                connection.query(sqlExist, this.pivaFarma, 
-                    (err, result)=>{
+                connection.query('INSERT INTO session SET ?', [this],
+                    (err) => {
                         if(err) reject(err)
-                        if(result.length==0) return reject()
 
-                        connection.query('INSERT INTO session SET ?', [this],
-                            (err) => {
-                                if(err) reject(err)
-
-                                resolve(this)
-                            });
-                    });
-                    connection.release()
+                        connection.release()
+                        resolve(this)
+                });
             })
         })
     }
 
     findById(){
         return new Promise( (resolve, reject) =>{
-            let sql = "SELECT * FROM session WHERE cfUtente = ? AND pivaFarma = ?";
+            let sql = "SELECT * FROM session WHERE id = ?";
 
             pool.getConnection( (err, connection) =>{
                 if(err) return reject(err);
 
-                connection.query(sql, [this.cfUtente, this.pivaFarma], 
+                connection.query(sql, [this.id], 
                     function(err, result){
                         if(err) return reject(err);
                         
@@ -59,12 +52,13 @@ class Session {
             pool.getConnection( (err, connection) =>{
                 if(err) return reject(err)
 
-                let sqlSession = "SELECT cfUtente, pivaFarma, time FROM session WHERE cfUtente = ? GROUP BY cfUtente, pivaFarma, time ORDER BY time DESC"
+                let sqlSession = "SELECT cfUtente, pivaFarma FROM session WHERE cfUtente = ? GROUP BY cfUtente, pivaFarma ORDER BY time DESC"
                 let sqlMsg = "SELECT * FROM msg WHERE cfUtente = ? AND pivaFarma = ? ORDER BY time DESC LIMIT 1";
 
                 connection.query(sqlSession, [this.cfUtente], 
                     function(err, sessions) {
                         if(err) reject (err)
+                        console.log(sessions)
                         Promise.all(sessions.map(({ cfUtente, pivaFarma, time }) => 
                             new Promise((resolve, reject) => {
                                 connection.query(sqlMsg, [cfUtente, pivaFarma, time],
@@ -128,14 +122,14 @@ class Session {
 
     findOpenSessionById(){
         return new Promise( (resolve, reject) => {
-            let sql = "SELECT * FROM session WHERE cfUtente = ? AND pivaFarma = ? AND status = 'open'";
+            let sql = "SELECT * FROM session WHERE id = ? AND status = 'open'";
 
             pool.getConnection( (err, connection) => {
-                if(err) return reject(err)
+                if(err) reject(err)
 
-                connection.query(sql, [this.cfUtente,this.pivaFarma],
+                connection.query(sql, [this.id],
                     function(err, result){
-                        if(err) return reject(err)
+                        if(err) reject(err)
 
                         connection.release()
                         resolve(result)
@@ -146,7 +140,7 @@ class Session {
 
     findOpenSessionByUser(){
         return new Promise((resolve, reject) => {
-            let sql = "SELECT * FROM session WHERE cfUtente = ? AND status = 'OPEN'";
+            let sql = "SELECT * FROM session WHERE cfUtente = ? AND status = 'open'";
 
 
             pool.getConnection((err, connection) => {
@@ -167,7 +161,7 @@ class Session {
 
     findOpenSessionByPharma(){
         return new Promise((resolve, reject)=>{
-            let sql = "SELECT * FROM session WHERE pivaFarma = ? AND status = 'OPEN'";
+            let sql = "SELECT * FROM session WHERE pivaFarma = ? AND status = 'open'";
 
             pool.getConnection((err, connection)=>{
                 if(err) return reject(err)
