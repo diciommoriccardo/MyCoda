722cba6d5b635bf39c9e537a201f6201475f5abe
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-07 18:33:03 +0200

    sessionClose route

---
+++ src/routes/session/sessionClose.js
@@ -0,0 +1,19 @@
+import Router from 'express';
+import Session from '../../models/session.model.js';
+
+const router = Router();
+router.patch('/close', (req, res) => {
+    const id = req.user.id;
+    const session = {
+        cfUtente: id
+    }
+
+    new Session(session)
+    .then(session => session.findOpenSessionByUser())
+    .then(result => new Session({id:result[0].id}))
+    .then(session => session.sessionClose())
+    .then(result => res.status(201).json(result))
+    .catch(err => res.status(500).json(err))
+})
+
+export default router
