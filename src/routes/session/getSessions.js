f3b8225f84a438ad712860d3a92c8c13c4ed7e4b
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-12 16:10:13 +0200

    changed result set and endpoint

--- src/routes/session/getSessions.js
+++ src/routes/session/getSessions.js
@@ -1,5 +1,7 @@
 import Router from 'express';
 import Session from '../../models/session.model.js';
+import Message from '../../models/message.model.js';
+import Pharmacy from '../../models/pharmacy.model.js';
 
 const router = Router();
 
@@ -10,9 +12,35 @@ router.get('', (req, res) => {
     };
     
     new Session( session )
-    .then(session => type === 'user' ? session.findByUser() : session.findByPharma())
-    .then(result => { return res.status(201).json(result); })
-    .catch(err => { return res.status(500).json({error: {message: err}})})
+    .then(session => type === 'user' ? session.findByUserNew() : session.findByPharmaNew())
+    .then(result => { 
+        console.log(result)
+        return Promise.all(result.map(({id, pivaFarma, cfUtente, time}) => {
+            return new Promise((resolve) => {
+                new Message({idSession: id})
+                .then(message => message.lastMessageBySession())
+                .then(last => {
+                    
+                    new Pharmacy({ piva: pivaFarma})
+                    .then(pharmacy => pharmacy.findByCf())
+                    .then(pharma => {console.log(pharma); resolve({
+                        cfUtente: cfUtente,
+                        pivaFarma: pivaFarma,
+                        ragSociale: pharma[0].ragSociale,
+                        time: time,
+                        message: {
+                            content: last[0].content,
+                            mittente: last[0].mittente,
+                            time: last[0].time
+                        }
+                    })})    
+                })
+                .catch(err => res.status(500).json(err))
+            })
+        }))
+     })
+    .then(result => res.status(201).json(result))
+    .catch(err => res.status(500).json({error: {message: err}}))
 
 })
 
