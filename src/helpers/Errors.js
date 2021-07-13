ef5bb55703dbe6c6be34aec67be8614e528576d2
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-13 21:12:09 +0200

    custom errors

--- src/helpers/Errors.js
+++ src/helpers/Errors.js
@@ -6,9 +6,16 @@ class Errors extends Error {
     }
 }
 
-class ResourceNotFound extends Errors {
+export class ResourceNotFound extends Errors {
     constructor(resource, query) {
         super(`Resource ${resource} was not found.`);
+        this.data = { resource, query };
+    }
+}
+
+export class InternalError extends Errors {
+    constructor(resource, query){
+        super(`Server Internal Error`);
         this.data = { resource, query };
     }
 }
