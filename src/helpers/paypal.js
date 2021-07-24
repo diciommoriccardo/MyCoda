ae81ae50e865fefa2043510750e5a9d4bd1c00d7
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-24 12:58:39 +0200

    paypal helper

--- src/helpers/paypal.js
+++ src/helpers/paypal.js
@@ -1,43 +1,62 @@
-import Paypal from 'paypal-rest-sdk';
+import Paypal from '@paypal/checkout-server-sdk';
 import {PAYPAL} from '../config/constants.js';
 
-Paypal.configure({
-    'mode': 'sandbox',
-    'client_id': PAYPAL.CLIENT_ID,
-    'client_secret': PAYPAL.SECRET
-})
+const CLIENT_ID = PAYPAL.CLIENT_ID;
+const SECRET = PAYPAL.SECRET;
 
-const payment = {
-    create: (amount, description) => {
-        return new Promise((resolve, reject) => {
-            var create_payment_json = {
-                "intent": "sale",
+const ENVIROMENT = new Paypal.core.SandboxEnvironment(CLIENT_ID, SECRET);
+const CLIENT = new Paypal.core.PayPalHttpClient(ENVIROMENT);
+let request = new Paypal.orders.OrdersCreateRequest();
+
+const Payment = {
+    create: (amount, payeeEmail) => {
+        return new Promise((resolve) => {
+            request.requestBody({
+                "intent": "CAPTURE",
                 "payer": {
                     "payment_method": "paypal"
                 },
-                "redirect_urls": {
-                    "return_url": "https://localhost:8080/api/payments/success",
-                    "cancel_url": "https://localhost:8080/api/payments/cancel"
+                "application_context": {
+                    "return_url": "https://api.server.mycoda.it/api/payments/success",
+                    "cancel_url": "https://api.server.mycoda.it/api/payments/cancel",
+                    "locale": "it-IT",
+                    "user_action": "CONTINUE"
                 },
-                "transactions": [{
-                    "amount": {
-                        "currency": "EUR",
-                        "total": amount
-                    },
-                    "description": description
-                }]
-            };
-            
-            
-            Paypal.payment.create(create_payment_json, function (error, payment) {
-                if (error) {
-                    reject(err)
-                } else {
-                    resolve(payment)
+                "purchase_units": [
+                    {
+                        "amount": {
+                            "currency_code": "USD",
+                            "value": amount
+                        }
+                    }
+                 ],
+                "payee": {
+                    "email_address": payeeEmail
                 }
-            });  
+            })
+                 
+            let response = CLIENT.execute(request)
+            resolve(response)
+        })
+    },
+
+    capture: (orderId) => {
+        return new Promise((resolve) => {
+            let request = new Paypal.orders.OrdersCaptureRequest(orderId);
+            request.requestBody({})
+
+            let response = CLIENT.execute(request);
+            resolve(response)
+        })
+    },
+    
+    getOrder: (orderId) => {
+        return new Promise((resolve, reject) => {
+            let request = new Paypal.orders.OrdersGetRequest(orderId);
+            let response = CLIENT.execute(request);
+            resolve(response)
         })
     }
-
-    
 }
+
+export default Payment;
