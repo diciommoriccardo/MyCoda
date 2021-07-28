621b8bd02650d232feaf10ba069c416810f10739
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-07-28 17:18:19 +0200

    get message route with payments

--- src/routes/messages/getMessages.js
+++ src/routes/messages/getMessages.js
@@ -1,17 +1,19 @@
 import Router from 'express';
 import Session from '../../models/session.model.js';
 import Message from '../../models/message.model.js';
+import Payment from '../../models/payment.model.js';
 
 const router = Router();
 
 router.get('/:id', (req, res) => {
-    const { id, type } = req.user;
+    const UserId = req.user.id;
+    const type  = req.user.type;
     const receiverId = req.params.id;
     const offset = req.query.offset && (parseInt(req.query.offset));
     const limit = req.query.limit && (parseInt(req.query.limit));
 
     const session = {
-        ...(type === 'user') ? {cfUtente: id, pivaFarma: receiverId} : {cfUtente: receiverId, pivaFarma: id}
+        ...(type === 'user') ? {cfUtente: UserId, pivaFarma: receiverId} : {cfUtente: receiverId, pivaFarma: UserId}
     };
 
     new Session( session )
@@ -19,8 +21,52 @@ router.get('/:id', (req, res) => {
     .then(result => new Message({ idSession: result[0].id, mittente: receiverId }))
     .then(message => message.changeStatusForSession())
     .then(message => message.findBySession(offset, limit))
-    .then(messages => res.status(200).json(messages))
-    .catch(err => res.status(500).json(err))
+    .then(messages => 
+        Promise.all(messages.map(({id, mittente, content, time, tipo, stato, idSession}) => {
+            return new Promise((resolve, reject) => {
+                switch (tipo) {
+                    case 3:
+                        const payment = {
+                            time: time,
+                            ...(type === 'user') ? {cfUtente: UserId, pivaFarma: receiverId} : {cfUtente: receiverId, pivaFarma: UserId}
+                        };
+                        new Payment(payment)
+                        .then(payment => payment.find())
+                        .then(result =>
+                            resolve({
+                                id,
+                                mittente,
+                                stato,
+                                idSession,
+                                payment: {
+                                    id: result[0].id,
+                                    receiverId: (type === 'user') ? result[0].cfUtente : result[0].pivaFarma,
+                                    somma: result[0].somma,
+                                    desc: result[0].desc,
+                                    stato: result[0].stato,
+                                    paypalId: result[0].paypalId
+                                }
+                        }))
+                        .catch(err => reject(err))
+                        break;
+                
+                    default:
+                        resolve({
+                            id, 
+                            mittente, 
+                            content, 
+                            time, 
+                            tipo, 
+                            stato,
+                            idSession
+                        })
+                        break;
+                }
+            })
+        }))   
+    )
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({error : {message: err}}))
 })
 
 export default router
