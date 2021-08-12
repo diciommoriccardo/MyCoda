69840799cddd005efd69e5609ef5fd83157a46d1
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-08-12 17:36:57 +0200

    fixed route

--- src/routes/session/sessionClose.js
+++ src/routes/session/sessionClose.js
@@ -2,18 +2,19 @@ import Router from 'express';
 import Session from '../../models/session.model.js';
 
 const router = Router();
-router.patch('/close', (req, res) => {
-    const id = req.user.id;
+router.post('/:id/close', (req, res) => {
+    const {id, type} = req.user;
+    const receiverId = req.params.id;
     const session = {
-        cfUtente: id
+        ...(type === 'user' ? {cfUtente: id, pivaFarma: receiverId} : {pivaFarma: id, cfUtente: receiverId})
     }
 
     new Session(session)
-    .then(session => session.findOpenSessionByUser())
+    .then(session => session.findOpenSessionByBoth())
     .then(result => new Session({id:result[0].id}))
     .then(session => session.sessionClose())
     .then(result => res.status(200).json(result))
-    .catch(err => res.status(500).json(err))
+    .catch(err => res.status(err.data.status).json({error: err.data}))
 })
 
 export default router
