81932869ecf0ead215933f50173cf09b3b767940
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-09-01 16:37:41 +0200

    changes route and session control

--- src/routes/images/upload.js
+++ src/routes/images/upload.js
@@ -2,43 +2,49 @@ import Router from 'express';
 import Message from '../../models/message.model.js';
 import multer from 'multer';
 import { s3Upload } from '../../helpers/aws.js';
+import Session from '../../models/session.model.js';
 
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-router.post('', upload.single('image'), (req, res) => {
-    const {id} = req.user;
+router.post('/upload/:id', upload.single('image'), (req, res) => {
+    const { id, type } = req.user;
+    const receiverId = req.params.id;
     const file = req.file;
-    const {data, idSession} = req.body;
 
-    s3Upload(file)
-    .then(data => {
-        console.log(data);
-        const location = `https://api.server.mycoda.it/api/images/${data.key}`;
+    const session = {
+        ...(type === 'user') ? { cfUtente: id, pivaFarma: receiverId} : {cfUtente: receiverId, pivaFarma: id}
+    }
 
-        const message = {
-            content: location,
-            tipo: 1,
-            mittente: id,
-            idSession: idSession
-        }
-
-        new Message(message)
-        .then(message => message.create())
-        .then(result => res.status(201).json({
-            image: {
-                key: data.key
-            },
-            message: result
-        }))
-    })
-    .catch(err => {
-        res.json({
-            message: err
+    new Session( session )
+    .then(session => session.create())
+    .then(session => {
+        s3Upload(file)
+        .then(data => {
+            const location = `https://api.server.mycoda.it/api/images/${data.key}`;
+    
+            const message = {
+                content: location,
+                tipo: 1,
+                mittente: id,
+                idSession: session.id
+            }
+            new Message(message)
+            .then(message => message.create())
+            .then(result => res.status(201).json({
+                image: {
+                    key: data.key
+                },
+                message: result
+            }))
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({
+                message: err
+            })
         })
     })
-
-    console.log(file);
 })
 
 export default router;
