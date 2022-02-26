486b9ebce72c01c404a193e07c8a02c6160c74a8
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-02-26 12:55:30 +0100

    changed body struct

--- src/routes/pharmacy/login.js
+++ src/routes/pharmacy/login.js
@@ -6,14 +6,14 @@ const router = Router();
 
 router.post('/login', function(req, res){
     if (!req.body) return res.status(400).json({error: {message: 'Content cannot be empty'}});
-    if (!req.body.piva || !req.body.password) 
+    if (!req.body.pIva || !req.body.password) 
         return res.status(400).json({error: {message: 'P.IVA and password are required'}});
 
     new Pharmacy ( req.body )
         .then(pharmacy => pharmacy.login())
         .then(row => {
-            const { piva, ragSociale, indirizzo, email, refresh_token } = row;
-            var accessToken = jwt.signAccessToken({ id: piva, type: "pharmacy" });
+            const { pIva, ragSociale, indirizzo, email, refresh_token } = row;
+            var accessToken = jwt.signAccessToken({ id: pIva, type: "pharmacy" });
             return res.status(200).json({
                 pIva,
                 ragSociale,
