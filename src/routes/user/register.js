b9902ebeb0219f53628633f4d9345be509e1bf25
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-04-04 13:03:03 +0200

    debug

--- src/routes/user/register.js
+++ src/routes/user/register.js
@@ -8,7 +8,7 @@ router.post('/register', function(req, res){
     if (!req.body) return res.status(400).json({error: {message: 'Content cannot be empty'}});
     req.body.cf = req.body.cf.toLowerCase();
     req.body.email = req.body.email.toLowerCase();
-    
+    console.log(req.body);
     new User (req.body)
     .then(user => user.register())
     .then(result => { return res.status(201).json({result, message: SUCCESS_ITA.REGISTER}) }) 
